d4253aeae3256243fa3fd4b1f4c389b90e35a39e
gt3oliveira <gt3oliveira@gmail.com>
2025-08-28 16:23:21 -0300

    feat: add checkbox terms to sign up form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Link } from 'react-router'
 
 import PasswordInput from '@/components/password-input'
@@ -10,6 +11,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 
 export function SignUpPage() {
@@ -26,6 +28,26 @@ export function SignUpPage() {
           <Input type="email" placeholder="Digite seu e-mail" />
           <PasswordInput />
           <PasswordInput placeholder="Confirme sua senha" />
+          <div className="items-top flex gap-3 rounded-lg border p-3 hover:bg-accent/50 has-[[aria-checked=true]]:border-green-600 has-[[aria-checked=true]]:bg-black dark:has-[[aria-checked=true]]:border-blue-900 dark:has-[[aria-checked=true]]:bg-blue-950">
+            <Checkbox
+              id="terms"
+              className="data-[state=checked]:border-green-600 data-[state=checked]:bg-green-600 data-[state=checked]:text-white dark:data-[state=checked]:border-green-700 dark:data-[state=checked]:bg-green-700"
+            />
+            <div className="mt-0.5 grid gap-1.5 font-normal">
+              <label
+                htmlFor="terms"
+                className="text-xs font-medium leading-none"
+              >
+                Li e concordo.
+              </label>
+              <p className="text-xs text-muted-foreground">
+                Ao clicar em "Cria conta", você concorda com os{' '}
+                <a href="#" className="underline">
+                  termos de uso e política de privacidade.
+                </a>
+              </p>
+            </div>
+          </div>
         </CardContent>
         <CardFooter>
           <Button className="w-full">Cria conta</Button>
