747d0f041e84dbe60f63c276fb17fb0f0998cb1d
gt3oliveira <gt3oliveira@gmail.com>
2025-08-29 15:53:07 -0300

    feat: add auth context e inseriu no main e no signup page

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
 import { Toaster } from './components/ui/sonner'
+import { AuthContextProvider } from './contexts/auth'
 import { HomePage } from './pages/Home'
 import { LoginPage } from './pages/Login'
 import { NotFoundPage } from './pages/NotFound'
@@ -16,14 +17,16 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContextProvider>
       <Toaster />
     </QueryClientProvider>
   </StrictMode>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useMutation } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router'
-import { toast } from 'sonner'
 import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
@@ -27,7 +25,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { api } from '@/lib/axios'
+import { AuthContext } from '@/contexts/auth'
 
 const signupSchema = z
   .object({
@@ -58,20 +56,7 @@ const signupSchema = z
   })
 
 export function SignUpPage() {
-  const [user, setUser] = useState(null)
-  const signupMutation = useMutation({
-    mutationKey: ['signup'],
-    mutationFn: async (variables) => {
-      const response = await api.post('/user', {
-        first_name: variables.firstName,
-        last_name: variables.lastName,
-        email: variables.email,
-        password: variables.password,
-      })
-
-      return response.data
-    },
-  })
+  const { user, signup } = useContext(AuthContext)
 
   const methods = useForm({
     resolver: zodResolver(signupSchema),
@@ -85,49 +70,10 @@ export function SignUpPage() {
     },
   })
 
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const accessToken = localStorage.getItem('accessToken')
-        const refreshToken = localStorage.getItem('refreshToken')
-        if (!accessToken && !refreshToken) return
-
-        const response = await api.get('/user/me', {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        setUser(response.data)
-      } catch (error) {
-        localStorage.removeItem('accessToken')
-        localStorage.removeItem('refreshToken')
-        console.error(error)
-      }
-    }
-
-    init()
-  }, [])
-
-  const handleSubmit = (data) => {
-    signupMutation.mutate(data, {
-      onSuccess: (createdUser) => {
-        const accessToken = createdUser.tokens.accessToken
-        const refreshToken = createdUser.tokens.refreshToken
-        setUser(createdUser)
-        localStorage.setItem('accessToken', accessToken)
-        localStorage.setItem('refreshToken', refreshToken)
-        toast.success('Conta criada com sucesso!')
-      },
-      onError: () => {
-        toast.error(
-          'Ocorreu um erro ao criar a conta. Tente novamente mais tarde.'
-        )
-      },
-    })
-  }
+  const handleSubmit = (data) => signup(data)
 
   if (user) {
-    return <h1>Bem-vindo, {user.first_name}!</h1>
+    return <div>Bem-vindo, {user}!</div>
   }
 
   return (
