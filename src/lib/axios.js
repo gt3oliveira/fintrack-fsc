d7898a82f03d03f9449c454bfe26825951decb11
gt3oliveira <gt3oliveira@gmail.com>
2025-08-30 16:48:19 -0300

    feat: garantir que o accessToken seja refeitor usando o refreshToken com erro 401

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -19,3 +19,46 @@ protectedApi.interceptors.request.use((request) => {
   }
   return request
 })
+
+protectedApi.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const request = error.config
+    const refreshToken = localStorage.getItem(
+      LOCAL_STORAGE_TOKENS_KEYS.refreshToken
+    )
+    if (!refreshToken) {
+      return Promise.reject(error)
+    }
+
+    if (
+      error.response.status === 401 &&
+      !request._retry &&
+      !request.url.includes('/user/refresh-token')
+    ) {
+      request._retry = true
+      try {
+        const response = await protectedApi.post('/user/refresh-token', {
+          refreshToken,
+        })
+        localStorage.setItem(
+          LOCAL_STORAGE_TOKENS_KEYS.accessToken,
+          response.data.accessToken
+        )
+        localStorage.setItem(
+          LOCAL_STORAGE_TOKENS_KEYS.refreshToken,
+          response.data.refreshToken
+        )
+
+        request.headers.Authorization = `Bearer ${response.data.accessToken}`
+        return protectedApi(request)
+      } catch (refreshError) {
+        localStorage.removeItem(LOCAL_STORAGE_TOKENS_KEYS.accessToken)
+        localStorage.removeItem(LOCAL_STORAGE_TOKENS_KEYS.refreshToken)
+        console.error(refreshError)
+      }
+    }
+
+    return Promise.reject(error)
+  }
+)
